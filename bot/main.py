8d40e5d39895a33fc5c185835fc14759971cb0f6
maxRakh <maxmaxutmax@gmail.com>
2023-06-27 14:06:21 +0300

    update main.py: add get_preorders_excel

--- bot/main.py
+++ bot/main.py
@@ -1,3 +1,9 @@
+import csv
+import io
+import os
+from datetime import datetime
+
+import requests
 from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters import Text
@@ -15,7 +21,7 @@ dp = Dispatcher(bot)
 @dp.message_handler(commands='start')
 async def start(message: types.Message) -> None:
     if message.from_user.id in users:
-        start_buttons = ["Предзаказы выгрузить", "Предазказы товара", "Добавить предзаказ", "Выгрузить предзаказы в   "]
+        start_buttons = ["Предзаказы выгрузить", "Предазказы товара", "Добавить предзаказ", "Выгрузить предзаказы в Excel"]
         keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
         keyboard.add(*start_buttons)
         await message.answer("Привет! Это база предзаказов! Чем помочь?", reply_markup=keyboard)
@@ -23,7 +29,7 @@ async def start(message: types.Message) -> None:
         await message.answer("Извините, но у вас нет доступа к боту.")
 
 
-@dp.message_handler((Text(equals="Предзаказы выгрузить")))
+@dp.message_handler(Text(equals="Предзаказы выгрузить"))
 async def get_preorders(message: types.Message) -> None:
     if message.from_user.id in users:
         preorders_menu = get_all_preorders_menu()
@@ -39,13 +45,13 @@ async def inline_keyboard_preorders_buttons(callback: types.CallbackQuery) -> No
     page_size = 10
 
     if callback.data == 'all_preorders':
-        data = api.get_all_data("preorder", page_number=page_number, page_size=page_size)
+        data = api.get_all_data("preorders", page_number=page_number, page_size=page_size)
     elif callback.data == 'all_active_preorders':
-        data = api.get_all_data("preorder", page_number=page_number, page_size=page_size, bought=False, canceled=False)
+        data = api.get_all_data("preorders", page_number=page_number, page_size=page_size, bought=False, canceled=False)
     elif callback.data == 'all_bought_preorders':
-        data = api.get_all_data("preorder", page_number=page_number, page_size=page_size, bought=True)
+        data = api.get_all_data("preorders", page_number=page_number, page_size=page_size, bought=True)
     elif callback.data == 'all_canceled_preorders':
-        data = api.get_all_data("preorder", page_number=page_number, page_size=page_size, canceled=True)
+        data = api.get_all_data("preorders", page_number=page_number, page_size=page_size, canceled=True)
 
     results = data['response_text']
     pages = data['pages']
@@ -55,6 +61,23 @@ async def inline_keyboard_preorders_buttons(callback: types.CallbackQuery) -> No
     keyboard.add(*page_buttons)
 
     await callback.message.answer(results, reply_markup=keyboard)
+
+
+@dp.message_handler(Text(equals='Выгрузить предзаказы в Excel'))
+async def get_preorders_excel(message: types.Message) -> None:
+    if message.from_user.id in users:
+        api = WebAppAPI("http://web-app:8000/api/v1")
+        file_csv = api.export_csv(endpoint='preorders')
+
+        with open(file_csv, 'rb') as file:
+            await bot.send_document(message.chat.id, file)
+
+        try:
+            os.remove(file_csv)
+        except OSError as ex:
+            print(f"Ошибка при удалении файла: {ex}")
+    else:
+        await message.answer("Извините, но у вас нет доступа к боту.")
 
 
 # @dp.message_handler(Text(equals=))# ИЗМЕНИТЬ
